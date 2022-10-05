1ea5735cd09b6053bba10874f22bd766baf3a509
Vladyslav <vladislav.mpm@gmail.com>
2022-10-05 16:46:59 +0200

    add: view binding and hide system bars

--- app/src/main/java/com/myapps/projectx/SignInActivity.kt
+++ app/src/main/java/com/myapps/projectx/SignInActivity.kt
@@ -2,10 +2,27 @@ package com.myapps.projectx
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.WindowInsetsControllerCompat
+import com.myapps.projectx.databinding.ActivitySignInBinding
 
 class SignInActivity : AppCompatActivity() {
+    private lateinit var binding: ActivitySignInBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_sign_in)
+        binding = ActivitySignInBinding.inflate(layoutInflater)
+        hideSystemsBars()
+        setContentView(binding.root)
+    }
+
+    private fun hideSystemsBars(){
+        val windowInsetsController =
+            ViewCompat.getWindowInsetsController(window.decorView) ?: return
+        windowInsetsController.systemBarsBehavior =
+            WindowInsetsControllerCompat.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
+        windowInsetsController.hide(WindowInsetsCompat.Type.systemBars())
     }
 }
