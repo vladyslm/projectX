40f466488d4d2c30409965bb968d6deb845f5067
Vladyslav <vladislav.mpm@gmail.com>
2022-10-13 19:19:57 +0200

    add: inbox message fragment and connect it to the nav controller

--- app/src/main/java/com/myapps/projectx/adapters/InboxAdapter.kt
+++ app/src/main/java/com/myapps/projectx/adapters/InboxAdapter.kt
@@ -1,15 +1,21 @@
 package com.myapps.projectx.adapters
 
+import android.annotation.SuppressLint
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.navigation.Navigation
 import androidx.recyclerview.widget.RecyclerView
 import com.myapps.projectx.R
 import com.myapps.projectx.data.inbox.InboxMessage
+import com.myapps.projectx.fragments.InboxFragmentDirections
 
 class InboxAdapter: RecyclerView.Adapter<InboxAdapter.ViewHolder>() {
 
+//    private lateinit var inboxMessageLayout: ConstraintLayout
     private var inboxList = emptyList<InboxMessage>()
 
     class ViewHolder(itemView: View): RecyclerView.ViewHolder(itemView){
@@ -31,12 +37,22 @@ class InboxAdapter: RecyclerView.Adapter<InboxAdapter.ViewHolder>() {
 
         holder.itemView.findViewById<TextView>(R.id.inboxMessageFrom).text = currentItem.from
         holder.itemView.findViewById<TextView>(R.id.inboxMessageDate).text = currentItem.date
+
+        holder.itemView.findViewById<ConstraintLayout>(R.id.inboxMessageLayout).setOnClickListener {
+            Log.d("message layout", "${currentItem.message} from: ${currentItem.from}")
+
+            val action = InboxFragmentDirections.actionInboxFragmentToInboxMessageFragment(currentItem.message, currentItem.from)
+            Navigation.findNavController(holder.itemView).navigate(action)
+
+        }
+
     }
 
     override fun getItemCount(): Int {
         return inboxList.size
     }
 
+    @SuppressLint("NotifyDataSetChanged")
     fun setData(inbox: List<InboxMessage>){
         this.inboxList = inbox
         notifyDataSetChanged()
