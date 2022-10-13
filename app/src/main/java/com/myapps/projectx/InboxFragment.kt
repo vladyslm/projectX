35a1d0dad0f759853ef7938ee06ef75647642e2f
Vladyslav <vladislav.mpm@gmail.com>
2022-10-13 04:25:25 +0200

    add: test db

--- app/src/main/java/com/myapps/projectx/InboxFragment.kt
+++ app/src/main/java/com/myapps/projectx/InboxFragment.kt
@@ -1,10 +1,19 @@
 package com.myapps.projectx
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.myapps.projectx.data.inbox.InboxMessage
+import com.myapps.projectx.data.inbox.InboxMessageViewModel
+import kotlin.math.log
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -17,43 +26,75 @@ private const val ARG_PARAM2 = "param2"
  * create an instance of this fragment.
  */
 class InboxFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+//    // TODO: Rename and change types of parameters
+//    private var param1: String? = null
+//    private var param2: String? = null
+//
+//    override fun onCreate(savedInstanceState: Bundle?) {
+//        super.onCreate(savedInstanceState)
+//        arguments?.let {
+//            param1 = it.getString(ARG_PARAM1)
+//            param2 = it.getString(ARG_PARAM2)
+//        }
+//    }
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+//    val  messages = arrayListOf<String>()
+
+    private lateinit var inboxViewModel: InboxMessageViewModel
+    lateinit var recyclerView: RecyclerView
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_inbox, container, false)
+        val view = inflater.inflate(R.layout.fragment_inbox, container, false)
+
+//        val messages = resources.getStringArray(R.array.messages).toList()
+//        val from = resources.getStringArray(R.array.from_inbox).toList()
+
+        val adapter = InboxAdapter()
+
+        recyclerView = view.findViewById(R.id.inboxRecyclerView)
+        recyclerView.adapter = adapter
+        recyclerView.layoutManager = LinearLayoutManager(requireContext())
+
+        inboxViewModel = ViewModelProvider(this).get(InboxMessageViewModel::class.java)
+
+        inboxViewModel.readAllMessages.observe(viewLifecycleOwner, Observer { message ->
+            adapter.setData(message)
+        })
+
+//        adapter.setData(messages)
+
+//        val addInbox = view.findViewById<Button>(R.id.addInbox)
+
+//        addInbox.setOnClickListener {
+//            val temp = inboxViewModel.addInboxMessage(InboxMessage(0, "lorem lorem 1", "barack obama", "10/13/2022", false))
+//            val msgs = inboxViewModel.readAllMessages
+////            Log.d("temp", msgs.value.toString())
+//        }
+
+        return view
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment InboxFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            InboxFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
-    }
+//    companion object {
+//        /**
+//         * Use this factory method to create a new instance of
+//         * this fragment using the provided parameters.
+//         *
+//         * @param param1 Parameter 1.
+//         * @param param2 Parameter 2.
+//         * @return A new instance of fragment InboxFragment.
+//         */
+//        // TODO: Rename and change types and number of parameters
+//        @JvmStatic
+//        fun newInstance(param1: String, param2: String) =
+//            InboxFragment().apply {
+//                arguments = Bundle().apply {
+//                    putString(ARG_PARAM1, param1)
+//                    putString(ARG_PARAM2, param2)
+//                }
+//            }
+//    }
 }
--- app/src/main/java/com/myapps/projectx/ProfileFragment.kt
+++ app/src/main/java/com/myapps/projectx/ProfileFragment.kt
@@ -1,10 +1,17 @@
 package com.myapps.projectx
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
+import android.widget.Toast
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import com.myapps.projectx.data.User
+import com.myapps.projectx.data.UserViewModel
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -17,43 +24,81 @@ private const val ARG_PARAM2 = "param2"
  * create an instance of this fragment.
  */
 class ProfileFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+//    // TODO: Rename and change types of parameters
+//    private var param1: String? = null
+//    private var param2: String? = null
+//
+//    override fun onCreate(savedInstanceState: Bundle?) {
+//        super.onCreate(savedInstanceState)
+//        arguments?.let {
+//            param1 = it.getString(ARG_PARAM1)
+//            param2 = it.getString(ARG_PARAM2)
+//        }
+//    }
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    private lateinit var userViewModel: UserViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile, container, false)
+        val view = inflater.inflate(R.layout.fragment_profile, container, false)
+
+        userViewModel = ViewModelProvider(this).get(UserViewModel::class.java)
+
+        val name = view.findViewById<TextView>(R.id.userName)
+        val email = view.findViewById<TextView>(R.id.userEmail)
+
+
+
+        userViewModel.getUser.observe(viewLifecycleOwner, Observer { user ->
+            if (!user.isEmpty()){
+                val fName = user[0].firstName
+                val lName = user[0].lastName
+                val fullName = "$fName $lName"
+                name.text = fullName
+                email.text = user[0].email
+            } else{
+                userViewModel.addUser(User(0, "user1", "user1", "user1@hiof.no"))
+                Toast.makeText(requireContext(), "Empty DB", Toast.LENGTH_SHORT).show()
+            }
+
+//            Log.d("Temp", user[0].firstName)
+
+        })
+
+//        temp.setOnClickListener {
+//
+////            val user = User(0, "John", "Snow", "bastard@hiof.no")
+////            userViewModel.addUser(user)
+//            userViewModel.getUser.observe(viewLifecycleOwner, Observer { user ->
+//                Log.d("Temp", user[0].firstName)
+//            })
+//            Toast.makeText(requireContext(), "Success", Toast.LENGTH_SHORT).show()
+//
+//        }
+
+        return view
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment ProfileFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            ProfileFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
-    }
+//    companion object {
+//        /**
+//         * Use this factory method to create a new instance of
+//         * this fragment using the provided parameters.
+//         *
+//         * @param param1 Parameter 1.
+//         * @param param2 Parameter 2.
+//         * @return A new instance of fragment ProfileFragment.
+//         */
+//        // TODO: Rename and change types and number of parameters
+//        @JvmStatic
+//        fun newInstance(param1: String, param2: String) =
+//            ProfileFragment().apply {
+//                arguments = Bundle().apply {
+//                    putString(ARG_PARAM1, param1)
+//                    putString(ARG_PARAM2, param2)
+//                }
+//            }
+//    }
 }
