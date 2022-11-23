690486602ecea2c4719c7d18acc36a82209066e4
Vladyslav <vladislav.mpm@gmail.com>
2022-11-23 21:18:06 +0100

    add: timestampToLocalDate function

--- app/src/main/java/com/myapps/projectx/libs/DateConverter.kt
+++ app/src/main/java/com/myapps/projectx/libs/DateConverter.kt
@@ -1,6 +1,10 @@
 package com.myapps.projectx.libs
 
+import java.sql.Timestamp
 import java.text.SimpleDateFormat
+import java.time.LocalDate
+import java.time.ZoneId
+import java.util.Date
 import java.util.Locale
 
 class DateConverter {
@@ -9,6 +13,11 @@ class DateConverter {
             val formatter = SimpleDateFormat(pattern, locale)
             val date = formatter.parse(dateStr)
             return date?.time
+        }
+
+        fun timestampToLocalDate(timestamp: Long): LocalDate {
+            val date = Date(timestamp)
+            return date.toInstant().atZone(ZoneId.systemDefault()).toLocalDate()
         }
     }
 
